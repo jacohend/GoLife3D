6c7393264af0c36f9424bb80c6ceb2bf44e8c24a
Jacob Henderson <jacohend89@gmail.com>
2025-05-08 07:55:01 -0400

    new fixes for voxel Game of Life

--- life.go
+++ life.go
@@ -1,271 +1,236 @@
+// main.go
 package main
 
 import (
-    "github.com/go-gl/gl"
-    "fmt"
-    "math"
-    "github.com/banthar/Go-SDL/sdl"
-    "os"
-    "math/rand"
-    "time"
-    "runtime"
-    "sync"
+	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"runtime"
+	"sync"
+	"time"
+
+	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
 )
 
 const (
-    SCREEN_WIDTH  = 1000
-    SCREEN_HEIGHT = 1000
-    SCREEN_BPP    = 32
-    SIZE = 100
+	SCREEN_WIDTH  = 1000 // 10 000 px windows often fail on modern drivers
+	SCREEN_HEIGHT = 1000
+	SIZE          = 100 // voxel grid edge length
 )
 
 var (
-    surface *sdl.Surface
-    X_AXIS, Y_AXIS, Z_AXIS float32
-    DIRECTION int
+	window                 *glfw.Window
+	X_AXIS, Y_AXIS, Z_AXIS float32
+	DIRECTION              int
+	universe               [SIZE][SIZE][SIZE]uint8
+	mu                     sync.Mutex
 )
 
-
-var universe[SIZE][SIZE][SIZE]uint8
-var mu sync.Mutex
-
-func Quit(status int) {
-    sdl.Quit()
-    os.Exit(status)
+func fatalIf(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func resizeWindow(width, height int) {
-    if height == 0 {
-        height = 1
-    }
-    gl.Viewport(0, 0, width, height)
-    gl.MatrixMode(gl.PROJECTION)
-    gl.LoadIdentity()
-    aspect := gl.GLdouble(width / height)
-    var fov, near, far gl.GLdouble
-    fov = 15.0
-    near = 0.1
-    far = 100.0
-    top := gl.GLdouble(math.Tan(float64(fov*math.Pi/360.0))) * near
-    bottom := -top
-    left := aspect * bottom
-    right := aspect * top
-    gl.Frustum(float64(left), float64(right), float64(bottom), float64(top), float64(near), float64(far))
-    gl.MatrixMode(gl.MODELVIEW)
-    gl.LoadIdentity()
+	if height == 0 {
+		height = 1
+	}
+	gl.Viewport(0, 0, int32(width), int32(height))
+
+	gl.MatrixMode(gl.PROJECTION)
+	gl.LoadIdentity()
+
+	aspect := float64(width) / float64(height)
+	fov, near, far := 15.0, 0.1, 100.0
+	top := math.Tan(fov*math.Pi/360.0) * near
+	bottom := -top
+	left := aspect * bottom
+	right := aspect * top
+	gl.Frustum(left, right, bottom, top, near, far)
+
+	gl.MatrixMode(gl.MODELVIEW)
+	gl.LoadIdentity()
 }
 
-func draw(i, j, k int){
-    newi := ((1.0 + (float32(i)+0.0)- (float32(SIZE)/2.0)) /float32(SIZE))
-    newj := ((1.0 + (float32(j)+0.0)- (float32(SIZE)/2.0)) /float32(SIZE))
-    newk := ((1.0 + (float32(k)+0.0)- (float32(SIZE)/2.0)) /float32(SIZE))
-    gl.Color3f(float32(float32(i)/float32(SIZE)),float32(float32(j)/float32(SIZE)),float32(float32(k)/float32(SIZE)))
-    gl.Vertex3f(newi, newj, newk)
+// --- drawing helpers -------------------------------------------------------
+
+func draw(i, j, k int) {
+	n := float32(SIZE)
+	newi := (1 + float32(i) - n/2) / n
+	newj := (1 + float32(j) - n/2) / n
+	newk := (1 + float32(k) - n/2) / n
+
+	gl.Color3f(float32(i)/n, float32(j)/n, float32(k)/n)
+	gl.Vertex3f(newi, newj, newk)
 }
 
 func drawGLScene() {
-    gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-    gl.LoadIdentity()
-    gl.Translatef(0.0, 0.0, -6.0)
-    gl.Rotatef(X_AXIS, 1.0, 0.0, 0.0)
-    gl.Rotatef(Y_AXIS, 0.0, 1.0, 0.0)
-    gl.Rotatef(Z_AXIS, 0.0, 0.0, 1.0)
-    gl.Begin(gl.POINTS)
-    gl.Color3f(0.0,0.0,0.0)
-    mu.Lock()
-    for x := range make([]int, SIZE){
-        for y := range make([]int, SIZE){
-            for z := range make([]int, SIZE){
-                if universe[x][y][z] == 1{
-                    draw(x, y, z)
-                }
-            }
-        }
-    }
-    mu.Unlock()
-    gl.End()
-    if X_AXIS > 360 || X_AXIS == 0 || X_AXIS < -360{
-        X_AXIS = 0.0
-    }else{
-        X_AXIS = X_AXIS + 0.3*float32(DIRECTION)
-    }
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.LoadIdentity()
 
-    if Y_AXIS > 360 || Y_AXIS == 0 || Y_AXIS < -360{
-        Y_AXIS = 0.0
-    }else{
-        Y_AXIS = Y_AXIS + 0.3*float32(DIRECTION)
-    }
+	gl.Translatef(0, 0, -6)
+	gl.Rotatef(X_AXIS, 1, 0, 0)
+	gl.Rotatef(Y_AXIS, 0, 1, 0)
+	gl.Rotatef(Z_AXIS, 0, 0, 1)
 
-    if Z_AXIS > 360 || Z_AXIS == 0 || Z_AXIS < -360{
-        Z_AXIS = 0.0
-    }else{
-        Z_AXIS = Z_AXIS + 0.3 * float32(DIRECTION)
-    }
-    sdl.GL_SwapBuffers()
-    fmt.Println("animate tick")
+	gl.Begin(gl.POINTS)
+	mu.Lock()
+	for x := 0; x < SIZE; x++ {
+		for y := 0; y < SIZE; y++ {
+			for z := 0; z < SIZE; z++ {
+				if universe[x][y][z] == 1 {
+					draw(x, y, z)
+				}
+			}
+		}
+	}
+	mu.Unlock()
+	gl.End()
+
+	// gentle spin
+	for _, axis := range []*float32{&X_AXIS, &Y_AXIS, &Z_AXIS} {
+		if *axis > 360 || *axis < -360 {
+			*axis = 0
+		}
+		*axis += 0.3 * float32(DIRECTION)
+	}
 }
 
-func handleKeyPress(keysym sdl.Keysym) {
-    switch keysym.Sym {
-    case sdl.K_ESCAPE:
-        Quit(0)
-    case sdl.K_F1:
-        sdl.WM_ToggleFullScreen(surface)
-    case sdl.K_a:
-        DIRECTION = -1
-        Y_AXIS = Y_AXIS - 0.30
-    case sdl.K_s:
-        DIRECTION = 1
-        X_AXIS = X_AXIS + 0.30
-    case sdl.K_d:
-        DIRECTION = 1
-        Y_AXIS = Y_AXIS + 0.30
-    case sdl.K_w:
-        DIRECTION = -1
-        X_AXIS = X_AXIS - 0.30
-    case sdl.K_q:
-        DIRECTION = 0
-    }
+// --- input / callbacks ------------------------------------------------------
+
+func handleKeyPress(key glfw.Key) {
+	switch key {
+	case glfw.KeyEscape:
+		window.SetShouldClose(true)
+	case glfw.KeyA:
+		DIRECTION = -1
+		Y_AXIS -= 0.30
+	case glfw.KeyS:
+		DIRECTION = 1
+		X_AXIS += 0.30
+	case glfw.KeyD:
+		DIRECTION = 1
+		Y_AXIS += 0.30
+	case glfw.KeyW:
+		DIRECTION = -1
+		X_AXIS -= 0.30
+	case glfw.KeyQ:
+		DIRECTION = 0
+	}
 }
 
+// --- simulation -------------------------------------------------------------
+
+func createUniverse() {
+	rand.Seed(time.Now().UnixNano())
+	for x := 0; x < SIZE; x++ {
+		for y := 0; y < SIZE; y++ {
+			for z := 0; z < SIZE; z++ {
+				universe[x][y][z] = uint8(rand.Intn(2))
+			}
+		}
+	}
+}
+
+func bruteNeighbors(i, j, k int) int {
+	neighbors := 0
+	for x := i - 1; x <= i+1; x++ {
+		for y := j - 1; y <= j+1; y++ {
+			for z := k - 1; z <= k+1; z++ {
+				if x == i && y == j && z == k {
+					continue
+				}
+				if x >= 0 && x < SIZE &&
+					y >= 0 && y < SIZE &&
+					z >= 0 && z < SIZE &&
+					universe[x][y][z] == 1 {
+					neighbors++
+				}
+			}
+		}
+	}
+	return neighbors
+}
+
+func simulate() {
+	temp := universe
+	for {
+		var next [SIZE][SIZE][SIZE]uint8
+		for x := 0; x < SIZE; x++ {
+			for y := 0; y < SIZE; y++ {
+				for z := 0; z < SIZE; z++ {
+					cell := temp[x][y][z]
+					n := bruteNeighbors(x, y, z)
+					if cell == 1 && (n < 4 || n > 5) {
+						next[x][y][z] = 0
+					} else if cell == 0 && (n == 4 || n == 5) {
+						next[x][y][z] = 1
+					} else {
+						next[x][y][z] = cell
+					}
+				}
+			}
+		}
+		mu.Lock()
+		universe = next
+		mu.Unlock()
+		temp = next
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+// --- main -------------------------------------------------------------------
 
 func initGL() {
-    gl.ShadeModel(gl.SMOOTH)
-    gl.ClearColor(0.0, 0.0, 0.0, 0.0)
-    gl.ClearDepth(1.0)
-    gl.Enable(gl.DEPTH_TEST)
-    gl.DepthFunc(gl.LESS)
-    gl.Hint(gl.PERSPECTIVE_CORRECTION_HINT, gl.NICEST)
+	gl.ShadeModel(gl.SMOOTH)
+	gl.ClearColor(0, 0, 0, 0)
+	gl.ClearDepth(1)
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LESS)
+	gl.Hint(gl.PERSPECTIVE_CORRECTION_HINT, gl.NICEST)
 }
 
-func create_universe(){
-    r := rand.New(rand.NewSource(99))
-    for x := range make([]int, SIZE){
-        for y := range make([]int, SIZE){
-            for z := range make([]int, SIZE){
-                if r.Intn(2) == 1{
-                    universe[x][y][z] = 1
-                }else{
-                    universe[x][y][z] = 0
-                }
-            }
-        }
-    }
-}
+func main() {
+	// OpenGL expects a single OS thread
+	runtime.LockOSThread()
 
-func bruteforce_neighbors(i, j, k int)(int){
-    neighbors:=0
-    for x:=i-1; x<i+2; x++{
-        for y:=j-1; y<j+2; y++{
-            for z:=k-1; z<k+2; z++{
-                if x==i && y==j && z==k{
-                    continue
-                }
-                if x > 0 && x < SIZE && y > 0 && y < SIZE && z > 0 && z < SIZE{
-                    if universe[x][y][z] == 1{
-                        neighbors += 1
-                    }
-                }
-            }
-        }
-    }
-    return neighbors
-}
+	fatalIf(glfw.Init())
+	defer glfw.Terminate()
 
-//this can be optimized using lookup tables and some clever heuristics
-func simulate(){
-    temp := universe
-    var chronon[SIZE][SIZE][SIZE] uint8
-    for{
-        for x := range make([]int, SIZE){
-            for y := range make([]int, SIZE){
-                for z := range make([]int, SIZE){
-                    chronon[x][y][z] = 0
-                    cell := temp[x][y][z]
-                    neighborhood:=bruteforce_neighbors(x,y,z)
-                    if cell == 1 && (neighborhood < 4 || neighborhood > 5){
-                        chronon[x][y][z] = 0
-                        continue
-                    }
-                    if cell == 0 && (neighborhood == 4 || neighborhood == 5){
-                        chronon[x][y][z] = 1
-                        continue
-                    }
-                }
-            }
-        }
-        temp = chronon
-        mu.Lock()
-        universe = temp
-        mu.Unlock()
-        time.Sleep(500 * time.Millisecond)
-        fmt.Println("universe tick")
-    }
-}
+	glfw.WindowHint(glfw.Resizable, glfw.True)
+	glfw.WindowHint(glfw.ContextVersionMajor, 2)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
 
+	var err error
+	window, err = glfw.CreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Voxel Universe", nil, nil)
+	fatalIf(err)
+	window.MakeContextCurrent()
+	glfw.SwapInterval(1) // vsync
 
-func main(){
+	fatalIf(gl.Init())
+	fmt.Println("OpenGL version:", gl.GoStr(gl.GetString(gl.VERSION)))
 
-    //use ALL the cores!
-    runtime.GOMAXPROCS(2)
-    // Initialize SDL
-    if sdl.Init(sdl.INIT_VIDEO) < 0 {
-        panic("Video initialization failed: " + sdl.GetError())
-    }
-    //if you wish to make an apple pie from scratch, you must first invent the universe
-    fmt.Println("create universe")
-    create_universe()
-    // flags to pass to sdl.SetVideoMode
-    videoFlags := sdl.OPENGL    // Enable OpenGL in SDL
-    videoFlags |= sdl.DOUBLEBUF // Enable double buffering
-    videoFlags |= sdl.HWPALETTE // Store the palette in hardware
-    videoFlags |= sdl.RESIZABLE // Enable window resizing
+	window.SetFramebufferSizeCallback(func(w *glfw.Window, width, height int) {
+		resizeWindow(width, height)
+	})
+	window.SetKeyCallback(func(w *glfw.Window, key glfw.Key, sc int, action glfw.Action, mods glfw.ModifierKey) {
+		if action == glfw.Press {
+			handleKeyPress(key)
+		}
+	})
 
-    // get a SDL surface
-    surface = sdl.SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, uint32(videoFlags))
+	initGL()
+	resizeWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-    // verify there is a surface
-    if surface == nil {
-        panic("Video mode set failed: " + sdl.GetError())
-        Quit(1)
-    }
+	createUniverse()
+	go simulate()
 
-    defer Quit(0)
-    sdl.GL_SetAttribute(sdl.GL_DOUBLEBUFFER, 1)
-    initGL()
-    resizeWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
-
-    //simulate universe continuously
-    fmt.Println("begin simulation")
-    go simulate()
-    running := true
-    isActive := true
-    for running {
-        for ev := sdl.PollEvent(); ev != nil; ev = sdl.PollEvent() {
-            switch e := ev.(type) {
-            case *sdl.ActiveEvent:
-                isActive = e.Gain != 0
-            case *sdl.ResizeEvent:
-                width, height := int(e.W), int(e.H)
-                surface = sdl.SetVideoMode(width, height, SCREEN_BPP, uint32(videoFlags))
-
-                if surface == nil {
-                    fmt.Println("Could not get a surface after resize:", sdl.GetError())
-                    Quit(1)
-                }
-                resizeWindow(width, height)
-            case *sdl.KeyboardEvent:
-                if e.Type == sdl.KEYDOWN {
-                    handleKeyPress(e.Keysym)
-                }
-            case *sdl.QuitEvent:
-                running = false
-            }
-        }
-
-        // draw the scene
-        if isActive {
-            drawGLScene()
-        }
-    }
+	for !window.ShouldClose() {
+		drawGLScene()
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
 }
